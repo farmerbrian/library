2db245456280481c2dc9b32e376ac2e1c9a6c01d
farmerbrian <nichols.brian.d@gmail.com>
2022-08-03 11:05:01 -0500

    Added Form and JS to add input to array

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 let myLibrary = [];
 
-function book(title, author, pages, read) {
+function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
-	this.sayTitle = function () {
-		console.log(title);
-	};
+	// this.sayTitle = function () {
+	// 	console.log(title);
+	// };
 }
 
 function addBookToLibrary() {
-	// do stuff here
+	let title = document.getElementById('title').value;
+	let author = document.getElementById('author').value;
+	let pages = document.getElementById('pages').value;
+	let read = document.getElementById('read').value;
+
+	const book = new Book(title, author, pages, read);
+	myLibrary.push(book);
+	document.forms[0].reset();
+	console.log(myLibrary);
+	document.getElementById('form').style.display = 'none';
+}
+
+function showForm() {
+	document.getElementById('form').style.display = 'block';
 }
